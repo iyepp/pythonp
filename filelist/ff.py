c79d813d64e9297dad5c5381e6aa21878be4781b
iyepp <iyepp@naver.com>
2019-12-26 15:44:56 +0900

    updated files in folders

--- filelist/ff.py
+++ filelist/ff.py
@@ -3,16 +3,35 @@
 from os.path import isfile, join
 from os import listdir
 
-path_dir="/home/jmkim/_Security/00.Fuzz/2018_Signage_Fuzz/2018_Signage_Fuzz_Data_Media/fuzzdata/avi"
+def searching(the_list, indent=False, level=0):
+    print( "\n"+ the_list )
 
-file_list = listdir(path_dir)
+    items = listdir(the_list)
+    
+    for each_item in items:
+        if isfile(join(the_list, each_item)):
+            if indent:
+                for tab_stop in range(level):
+                    print("\t", end='')
+                print(each_item)
+        else:
+            searching(join(the_list, each_item), indent, level+1)
 
-file_list.sort() #정렬도 가능하다
 
-for myfile in file_list:
-    print(isfile(join(path_dir,myfile)), end=" ")
-    if isfile(join(path_dir,myfile)):
-        print(myfile + " is FILE")
-    else:
-        print(myfile + " is DIR")
+
+path_dir="/home/jmkim/_Security/00.Fuzz/2018_Signage_Fuzz/2018_Signage_Fuzz_Data_Media/fuzzdata"
+
+mylist = listdir(path_dir) # ['bmp', 'rm', '3gp', 'mp4', 'jpg', 'png', 'avi', 'mkv']
+
+mylist.sort() #정렬도 가능하다
+
+for mypath in mylist:
+    #print(join(path_dir, mypath))
+    
+    searching(join(path_dir, mypath), True, 1)
+    #print(isfile(join(path_dir, myfile)), end=" ")
+    #if isfile(join(path_dir, myfile)):
+    #    print(myfile + " is FILE")
+    #else:
+    #    print(myfile + " is DIR")
 
